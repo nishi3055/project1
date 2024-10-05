7c73eb9da24ec10ad82eb0b0d89e87b1586977b7
User Grayturtle9 <grayturtle9@www1349.sakura.ne.jp>
2024-10-05 20:54:51 +0900

    restart

--- database/migrations/2024_10_05_005915_create_events_table.php
+++ database/migrations/2024_10_05_005915_create_events_table.php
@@ -8,15 +8,20 @@ class AddDetailToEventsTable extends Migration
 {
     public function up()
     {
-        Schema::table('events', function (Blueprint $table) {
-            $table->text('detail')->nullable();
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
     public function down()
     {
+<<<<<<< HEAD:database/migrations/2024_09_20_215745_add_detail_to_events_table.php
         Schema::table('events', function (Blueprint $table) {
             $table->dropColumn('detail');
         });
+=======
+        Schema::dropIfExists('events');
+>>>>>>> accbb83 (restart):database/migrations/2024_10_05_005915_create_events_table.php
     }
 }
